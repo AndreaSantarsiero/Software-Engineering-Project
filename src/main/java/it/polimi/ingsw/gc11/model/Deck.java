bb9a16d873098ccba85b5bae05fea3c6dcc7f17b
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-03-05 18:22:13 +0100

    Revert "Added attributes and methods for Deck class"
    This reverts commit b5ef861731bc80df2b087716daa6b31070f708ae.

--- src/main/java/it/polimi/ingsw/gc11/model/Deck.java
+++ src/main/java/it/polimi/ingsw/gc11/model/Deck.java
@@ -2,12 +2,10 @@ package it.polimi.ingsw.gc11.model;
 
 import it.polimi.ingsw.gc11.model.adventurecard.AdventureCard;
 
-import java.util.Stack;
-
 public class Deck {
-    private Stack<AdventureCard> cardsDeck;
+    private AdventureCard[] cards;
+
     public void shuffle() {}
 
-    public AdventureCard getTopCard() {
-        return cardsDeck.pop();   }
+    public void getTopCard() {}
 }
