8ae5341057f9bf9c5b2a6ea79eb004697f3bb4b3
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-03-05 18:27:51 +0100

    attribute name changed

--- src/main/java/it/polimi/ingsw/gc11/model/Deck.java
+++ src/main/java/it/polimi/ingsw/gc11/model/Deck.java
@@ -5,11 +5,11 @@ import it.polimi.ingsw.gc11.model.adventurecard.AdventureCard;
 import java.util.Stack;
 
 public class Deck {
-    private Stack<AdventureCard> cardsDeck;
+    private Stack<AdventureCard> cards;
 
     public void shuffle() {}
 
     public AdventureCard getTopCard() {
-        return cardsDeck.pop();
+        return cards.pop();
     }
 }
