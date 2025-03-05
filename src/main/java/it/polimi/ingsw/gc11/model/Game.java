8e7e78a79ad6356a9b8faedb3eb20471d6bbab38
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-03-05 18:47:48 +0100

    Added: Game attributes, constructor and methods

--- src/main/java/it/polimi/ingsw/gc11/model/Game.java
+++ src/main/java/it/polimi/ingsw/gc11/model/Game.java
@@ -2,9 +2,25 @@ package it.polimi.ingsw.gc11.model;
 
 public class Game {
     private String id;
+    private Player[] players;
+    private FlightBoard flightBoard;
+    private Deck[] decks;
 
+    public void start() {}
+    public void end() {}
 
-//    public int getNumPlayers() {
-//
-//    }
+    public Game(String id, Player[] players, FlightBoard.Type flightType) {
+        this.id = id;
+        this.players = players;
+        this.flightBoard = new FlightBoard(flightType);
+        this.decks = new Deck[3];
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getNumPlayers() {
+        return players.length;
+    }
 }
