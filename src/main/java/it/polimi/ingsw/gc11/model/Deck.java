ce704298f52640f8d47a4c31e2fdc899a605789b
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-03-05 18:24:01 +0100

    Deck implemented as a Stack of AdventureCards

--- src/main/java/it/polimi/ingsw/gc11/model/Deck.java
+++ src/main/java/it/polimi/ingsw/gc11/model/Deck.java
@@ -2,10 +2,14 @@ package it.polimi.ingsw.gc11.model;
 
 import it.polimi.ingsw.gc11.model.adventurecard.AdventureCard;
 
+import java.util.Stack;
+
 public class Deck {
-    private AdventureCard[] cards;
+    private Stack<AdventureCard> cardsDeck;
 
     public void shuffle() {}
 
-    public void getTopCard() {}
+    public AdventureCard getTopCard() {
+        return cardsDeck.pop();
+    }
 }
