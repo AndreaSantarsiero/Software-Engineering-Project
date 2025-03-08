476b2daac9ee1fdbd08583cc89070349c1f1ff6f
andreasantarsiero <andrea@aka-Slayer>
2025-03-08 13:31:24 +0100

    test structure added

---
+++ src/test/java/it/polimi/ingsw/gc11/model/shipboard/Level3ShipBoardTest.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.gc11.model.shipboard;
+
+public class Level3ShipBoardTest {
+}
---
+++ src/test/java/it/polimi/ingsw/gc11/model/shipboard/ShipBoardTest.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.gc11.model.shipboard;
+
+public class ShipBoardTest {
+}
