d8659505f0066533f62e2cc98d50618710a668c4
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-03-08 22:11:38 +0100

    Planet: materials attribute and its getter/setter added

--- src/main/java/it/polimi/ingsw/gc11/model/Planet.java
+++ src/main/java/it/polimi/ingsw/gc11/model/Planet.java
@@ -1,16 +1,20 @@
 package it.polimi.ingsw.gc11.model;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class Planet {
     private Boolean visited;
-
+    private Vector<Material> materials;
 
     public Boolean isVisited() {
         return visited;
     }
+    public void setVisited() { this.visited = true; }
 
+    public void setMaterials(Vector<Material> materials){ this.materials = materials;}
     public Vector<Material> getMaterials() {
-        return new Vector<Material>();
+        Vector<Material> materialsCopy = new Vector<>(materials);
+        return materialsCopy;
     }
 }
