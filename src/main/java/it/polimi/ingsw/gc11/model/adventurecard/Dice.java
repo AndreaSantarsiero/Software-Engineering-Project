ae25a907110c0b535b4cce45dddfb87b8ba5dd39
lorenzostani <lorenzo.stani.03@gmail.com>
2025-03-09 16:19:36 +0100

    updated Dice class

--- src/main/java/it/polimi/ingsw/gc11/model/adventurecard/Dice.java
+++ src/main/java/it/polimi/ingsw/gc11/model/adventurecard/Dice.java
@@ -3,26 +3,16 @@ package it.polimi.ingsw.gc11.model.adventurecard;
 import java.util.Random;
 
 public class Dice {
-    private int firstDice;
-    private int secondDice;
-    private Random random;
+    private int dice;
+    private final Random random;
 
-    // Metodo per tirare entrambi i dadi
-    public void roll() {
-        random = new Random();
-        firstDice = random.nextInt(6) + 1;
-        secondDice = random.nextInt(6) + 1;
+    public Dice() {
+        this.dice = 0;
+        this.random = new Random();
     }
 
-    // Restituisce il valore del primo dado,
-    // Prima di chiamare questo metodo va per forza chiamato Roll
-    public int getFirstDice() {
-        return firstDice;
-    }
-
-    // Restituisce il valore del secondo dado
-    // Prima di chiamare questo metodo va per forza chiamato Roll
-    public int getSecondDice() {
-        return secondDice;
+    public int roll() {
+        dice = random.nextInt(6) + 1;
+        return dice;
     }
 }
