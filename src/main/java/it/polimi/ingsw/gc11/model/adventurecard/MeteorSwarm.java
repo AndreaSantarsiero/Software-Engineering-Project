0844912a2ab098d18b6e9b4c946bbd531215cd71
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-03-12 21:22:21 +0100

    MeteorSwarm: constructor

--- src/main/java/it/polimi/ingsw/gc11/model/adventurecard/MeteorSwarm.java
+++ src/main/java/it/polimi/ingsw/gc11/model/adventurecard/MeteorSwarm.java
@@ -1,10 +1,22 @@
 package it.polimi.ingsw.gc11.model.adventurecard;
 
+import it.polimi.ingsw.gc11.model.Meteor;
 
+import java.util.ArrayList;
 
 public class MeteorSwarm extends AdventureCard {
+    private ArrayList<Meteor> meteors;
 
-    public MeteorSwarm(AdventureCard.Type type) {
+    //can't use parameters to instance meteors inside the constructor because
+    //they have too many attributes to describe them, easier with a list...
+    // ...maybe it's ok with json file??
+    public MeteorSwarm(AdventureCard.Type type, ArrayList<Meteor> meteors) {
         super(type);
+        this.meteors = meteors;
+    }
+
+    @Override
+    public void handler() {
+        //to implement
     }
 }
