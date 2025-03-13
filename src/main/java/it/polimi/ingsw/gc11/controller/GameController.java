ab812f64ce5ab3bfec9014cb74896bf61647dd01
Luca Sartori <luca.sartori1@outlook.com>
2025-03-13 15:20:38 +0100

    Delete Sabotage

--- src/main/java/it/polimi/ingsw/gc11/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/gc11/controller/GameController.java
@@ -1,15 +1,10 @@
 package it.polimi.ingsw.gc11.controller;
 
 import it.polimi.ingsw.gc11.model.GameModel;
-import it.polimi.ingsw.gc11.model.adventurecard.Sabotage;
 import it.polimi.ingsw.gc11.view.GameView;
 
 public class GameController {
     GameModel model;
     GameView view;
-
-    public void handler(Sabotage adventureCard){
-
-    }
 
 }
