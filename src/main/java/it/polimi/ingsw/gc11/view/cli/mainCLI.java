d364a6ba526f38e81b260f4bc7e6786a59f50b50
lorenzostani <lorenzo.stani.03@gmail.com>
2025-03-31 13:11:39 +0200

    added another ship board json

--- src/main/java/it/polimi/ingsw/gc11/view/cli/mainCLI.java
+++ src/main/java/it/polimi/ingsw/gc11/view/cli/mainCLI.java
@@ -7,7 +7,7 @@ import it.polimi.ingsw.gc11.model.shipboard.ShipBoard;
 
 public class mainCLI {
     public static void main(String[] args) {
-        ShipBoardLoader shipBoardLoader = new ShipBoardLoader("src/test/resources/it/polimi/ingsw/gc11/shipBoards/shipBoard4.json");
+        ShipBoardLoader shipBoardLoader = new ShipBoardLoader("src/test/resources/it/polimi/ingsw/gc11/shipBoards/shipBoard5.json");
         ShipBoard shipBoard = shipBoardLoader.getShipBoard();
         CLI.printShipBoard(shipBoard);
         System.out.println("Exposed connectors: " + shipBoard.getExposedConnectors());
