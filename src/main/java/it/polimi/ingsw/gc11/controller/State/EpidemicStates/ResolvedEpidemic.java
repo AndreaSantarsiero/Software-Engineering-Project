a0f6599ea4f5c97830e527e84599c1c12ce2852f
Luca Sartori <luca.sartori1@outlook.com>
2025-04-15 10:10:56 +0200

    ResolvedEpidemic State

--- src/main/java/it/polimi/ingsw/gc11/controller/State/EpidemicStates/ResolvedEpidemic.java
+++ src/main/java/it/polimi/ingsw/gc11/controller/State/EpidemicStates/ResolvedEpidemic.java
@@ -1,4 +1,17 @@
 package it.polimi.ingsw.gc11.controller.State.EpidemicStates;
 
+import it.polimi.ingsw.gc11.model.Player;
+
 public class ResolvedEpidemic {
+    Player player;
+    public ResolvedEpidemic(Player player) {
+        if(player==null){
+            throw new NullPointerException("player");
+        }
+        this.player = player;
+    }
+
+    public void killMembers(){
+        player.getShipBoard().epidemic();
+    }
 }
