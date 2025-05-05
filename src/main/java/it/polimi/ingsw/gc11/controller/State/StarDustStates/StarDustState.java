bb45b919dc2cf6c4111adc49b89466669a7a9998
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-05-05 20:24:14 +0200

    StarDust States : finished

--- src/main/java/it/polimi/ingsw/gc11/controller/State/StarDustStates/StarDustState.java
+++ src/main/java/it/polimi/ingsw/gc11/controller/State/StarDustStates/StarDustState.java
@@ -2,9 +2,22 @@ package it.polimi.ingsw.gc11.controller.State.StarDustStates;
 
 import it.polimi.ingsw.gc11.controller.State.AdventurePhase;
 import it.polimi.ingsw.gc11.controller.State.AdventureState;
+import it.polimi.ingsw.gc11.controller.State.IdleState;
+import it.polimi.ingsw.gc11.model.GameModel;
+import it.polimi.ingsw.gc11.model.Player;
+
+import java.util.ArrayList;
 
 public class StarDustState extends AdventureState {
     public StarDustState(AdventurePhase advContext) {
         super(advContext);
+        GameModel gameModel = this.advContext.getGameModel();
+        ArrayList<Player> reverseOrderPlayers = (ArrayList<Player>) gameModel.getPlayers().reversed();
+        for (Player player : reverseOrderPlayers) {
+            int numExposedConnectors = player.getShipBoard().getExposedConnectors();
+            String username = player.getUsername();
+            gameModel.move(username, numExposedConnectors);
+        }
+        this.advContext.setAdvState(new IdleState(advContext));
     }
 }
