40242bcfba7a502c50489edc4513a9a9859a7f63
andreasantarsiero <andrea@aka-Slayer>
2025-05-07 17:52:35 +0200

    username test added

--- src/test/java/it/polimi/ingsw/gc11/controller/network/ConnectionTest.java
+++ src/test/java/it/polimi/ingsw/gc11/controller/network/ConnectionTest.java
@@ -2,23 +2,25 @@ package it.polimi.ingsw.gc11.controller.network;
 
 import it.polimi.ingsw.gc11.controller.ServerController;
 import it.polimi.ingsw.gc11.controller.network.client.VirtualServer;
+import it.polimi.ingsw.gc11.exceptions.UsernameAlreadyTakenException;
 import it.polimi.ingsw.gc11.model.FlightBoard;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 
 public class ConnectionTest {
 
-    ServerController serverController;
-    String serverIp = "127.0.0.1";
-    int RMIPort = 1099;
-    int SocketPort = 1234;
+    static ServerController serverController;
+    static String serverIp = "127.0.0.1";
+    static int RMIPort = 1099;
+    static int SocketPort = 1234;
 
 
 
-    @BeforeEach
-    void setUp() throws InterruptedException {
+    @BeforeAll
+    static void setUp() throws InterruptedException {
         serverController = new ServerController(RMIPort, SocketPort);
         Thread.sleep(1000);  //waiting for the server to start up
         System.err.println("Server started on RMI port: " + RMIPort + ", Socket port: " + SocketPort);
@@ -32,5 +34,13 @@ public class ConnectionTest {
         playerUno.createMatch(FlightBoard.Type.LEVEL2);
         VirtualServer playerDue = new VirtualServer(Utils.ConnectionType.RMI, "playerDue", serverIp, RMIPort);
         playerDue.createMatch(FlightBoard.Type.TRIAL);
+    }
+
+
+
+    @Test
+    void testUniqueUsername() {
+        VirtualServer playerUno = new VirtualServer(Utils.ConnectionType.RMI, "username", serverIp, RMIPort);
+        assertThrows(UsernameAlreadyTakenException.class, () -> new VirtualServer(Utils.ConnectionType.RMI, "username", serverIp, RMIPort), "Username already taken");
     }
 }
