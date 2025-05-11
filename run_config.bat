3aaf893344795062495b151c334a2e55cafd2df7
andreasantarsiero <andrea@aka-Slayer>
2025-05-11 23:14:15 +0200

    automatic testing setup added

---
+++ run_config.bat
@@ -0,0 +1,14 @@
+@echo off
+
+:: Set the encoding to UTF-8 for all terminals
+chcp 65001
+
+:: Run the first terminal for ServerMAIN
+start cmd /k "java -cp target\GC11-1.0-SNAPSHOT-jar-with-dependencies.jar it.polimi.ingsw.gc11.controller.ServerMAIN"
+
+:: Run the second terminal for MainCLI with multiple-client activation key F1
+start cmd /k "java -cp target\GC11-1.0-SNAPSHOT-jar-with-dependencies.jar it.polimi.ingsw.gc11.view.cli.MainCLI -mc f1"
+
+:: Run the third terminal for MainCLI with multiple-client activation key F2
+start cmd /k "java -cp target\GC11-1.0-SNAPSHOT-jar-with-dependencies.jar it.polimi.ingsw.gc11.view.cli.MainCLI -mc f2"
+
