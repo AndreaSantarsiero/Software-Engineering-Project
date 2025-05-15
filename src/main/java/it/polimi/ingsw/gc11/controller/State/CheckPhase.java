1a26b1e7130b93155b5c6444e8cb88d3c68bdceb
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-05-15 14:54:41 +0200

    CheckPhase : updated and evolves correctly in case all shipboard are correct

--- src/main/java/it/polimi/ingsw/gc11/controller/State/CheckPhase.java
+++ src/main/java/it/polimi/ingsw/gc11/controller/State/CheckPhase.java
@@ -1,15 +1,38 @@
 package it.polimi.ingsw.gc11.controller.State;
 
 import it.polimi.ingsw.gc11.controller.GameContext;
+import it.polimi.ingsw.gc11.model.GameModel;
+import it.polimi.ingsw.gc11.model.Player;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CheckPhase extends GamePhase {
-    private GameContext gameContext;
+    private final GameContext gameContext;
+    private List<Player> badShipPlayers;
+    private int numShipsCorrected;
 
     public CheckPhase(GameContext gameContext) {
         this.gameContext = gameContext;
+        this.badShipPlayers = new ArrayList<>();
+        this.numShipsCorrected = 0;
+
+        GameModel gameModel = gameContext.getGameModel();
+        List<Player> players = gameModel.getPlayers();
+        for (Player player : players) {
+            if(!player.getShipBoard().checkShip()){
+                this.badShipPlayers.add(player);
+            }
+        }
+
+        if (this.badShipPlayers.isEmpty()) {
+            gameContext.setPhase(new AdventurePhase(this.gameContext));
+        }
     }
 
-    public void nextPhase() {
-        gameContext.setPhase(new AdventurePhase(this.gameContext));
+    //Immagino che al player vengano proposte diverse scelte per riparare la nave, lui ne sceglie una alla volta fino a
+    //che non sistema la nave
+    public void repairShip(String username, int choice){
+
     }
+
 }
