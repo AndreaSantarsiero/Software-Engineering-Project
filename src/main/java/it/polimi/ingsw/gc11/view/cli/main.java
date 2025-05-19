5f3f57a7420fb52b056b165b3d17bc9733ceb05a
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-05-19 21:21:09 +0200

    fix

---
+++ src/main/java/it/polimi/ingsw/gc11/view/cli/main.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.gc11.view.cli;
+
+public class main {
+
+    public static void main(String[] args) {
+        MainCLI mainCLI = new MainCLI();
+        mainCLI.run(args);
+    }
+}
