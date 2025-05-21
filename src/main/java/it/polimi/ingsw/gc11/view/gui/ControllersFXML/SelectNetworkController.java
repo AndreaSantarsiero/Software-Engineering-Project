f7cec4b7593945aadaae0cab1fcf21e71f21dfec
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-05-21 13:32:38 +0200

    Gui SelectNetwork

---
+++ src/main/java/it/polimi/ingsw/gc11/view/gui/ControllersFXML/SelectNetworkController.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.gc11.view.gui.ControllersFXML;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SelectNetworkController {
+
+    @FXML
+    private Button rmiButton;
+
+    @FXML
+    private Button socketButton;
+
+    @FXML
+    protected void onRMIButtonClick(ActionEvent event) {
+        System.out.println("RMI selected");
+    }
+
+    @FXML
+    protected void onSOCKETButtonClick(ActionEvent event) {
+        System.out.println("SOCKET selected");
+    }
+
+
+}
--- src/main/java/it/polimi/ingsw/gc11/view/gui/ControllersFXML/SplashScreenController.java
+++ src/main/java/it/polimi/ingsw/gc11/view/gui/ControllersFXML/SplashScreenController.java
@@ -1,14 +1,33 @@
 package it.polimi.ingsw.gc11.view.gui.ControllersFXML;
 
+import it.polimi.ingsw.gc11.view.gui.MainGUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class SplashScreenController {
-
+    @FXML
+    Button playButton;
 
     @FXML
     protected void onPlayButtonClick(ActionEvent event) {
         System.out.println("onPlayButtonClick");
+
+        Stage stage = (Stage) playButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainGUI.class.getResource("/it/polimi/ingsw/gc11/gui/SelectNetwork.fxml"));
+        try {
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
