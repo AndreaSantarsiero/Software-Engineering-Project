81ae6e0a94c3d3e2bb328a61be41aad5bba4e585
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-05-23 19:07:54 +0200

    Gui Login + Create/Join match

--- src/main/java/it/polimi/ingsw/gc11/view/gui/ControllersFXML/LoginController.java
+++ src/main/java/it/polimi/ingsw/gc11/view/gui/ControllersFXML/LoginController.java
@@ -1,29 +1,35 @@
 package it.polimi.ingsw.gc11.view.gui.ControllersFXML;
 
 import it.polimi.ingsw.gc11.controller.network.client.VirtualServer;
+import it.polimi.ingsw.gc11.view.gui.MainGUI;
 import it.polimi.ingsw.gc11.view.gui.ViewModel;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class LoginController {
 
     @FXML
     private Button enterButton;
-
-    @FXML
-    private Button nextButton;
-
     @FXML
     private TextField usernameText;
-
+    @FXML
+    private HBox status;
     @FXML
     private Label label2;
-
+    @FXML
+    private HBox match;
+    @FXML
+    private Button join;
+    @FXML
+    private Button create;
 
     @FXML
     protected void onEnterButtonClick(ActionEvent event) {
@@ -37,18 +43,30 @@ public class LoginController {
         try {
             virtualServer.registerSession(username);
             viewModel.setMyself(username);
-            nextButton.setVisible(true);
+            enterButton.setVisible(false);
 
-            label2.setText("You are logged in as " + username);
+            status.setVisible(true);
+            label2.setText("You are logged in as: " + username);
             label2.setStyle("-fx-text-fill: green;" + label2.getStyle());
-            System.out.println("You are logged in as " + username);
+            System.out.println("You are logged in as: " + username);
+
+            match.setVisible(true);
         }
         catch (Exception e) {
-
             label2.setText("Error");
             label2.setStyle("-fx-text-fill: red;" + label2.getStyle());
             System.out.println(e.getMessage());
         }
+
+    }
+
+    @FXML
+    protected void onCreateMatchClick(ActionEvent event){
+
+    }
+
+    @FXML
+    protected void onJoinMatchClick(ActionEvent event){
 
     }
 }
