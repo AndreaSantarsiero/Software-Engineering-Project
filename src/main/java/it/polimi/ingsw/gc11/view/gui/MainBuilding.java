478342a1ed7e10d459f7665be8781189462889d9
lorenzostani <lorenzo.stani.03@gmail.com>
2025-05-25 15:59:37 +0200

    working on gui

--- src/main/java/it/polimi/ingsw/gc11/view/gui/MainBuilding.java
+++ src/main/java/it/polimi/ingsw/gc11/view/gui/MainBuilding.java
@@ -10,31 +10,31 @@ import java.io.IOException;
 import java.net.URL;
 
 
-public class MainBuilding /*extends Application*/ {
+public class MainBuilding extends Application {
 
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//
-//    private ViewModel viewModel;
-//
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        this.viewModel = new ViewModel();
-//        stage.setTitle("Galaxy Trucker");
-//
-//        FXMLLoader loader = new FXMLLoader(
-//                MainGUI.class.getResource("/it/polimi/ingsw/gc11/gui/buildingLV1.fxml")
-//        );
-//        Parent root = loader.load();
-//        Scene scene = new Scene(root, 1024, 768);
-//        stage.setUserData(this.viewModel);
-//
-//        stage.setFullScreen(true);
-//        stage.setScene(scene);
-//        stage.show();
-//
-//        BuildingController ctrl = loader.getController();
-//        ctrl.getSplitPane().setDividerPositions(0.79);
-//    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private ViewModel viewModel;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        this.viewModel = new ViewModel();
+        stage.setTitle("Galaxy Trucker");
+
+        FXMLLoader loader = new FXMLLoader(
+                MainGUI.class.getResource("/it/polimi/ingsw/gc11/gui/buildingLV1.fxml")
+        );
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 1024, 768);
+        stage.setUserData(this.viewModel);
+
+        stage.setFullScreen(true);
+        stage.setScene(scene);
+        stage.show();
+
+        BuildingController ctrl = loader.getController();
+        ctrl.getSplitPane().setDividerPositions(0.79);
+    }
 }
