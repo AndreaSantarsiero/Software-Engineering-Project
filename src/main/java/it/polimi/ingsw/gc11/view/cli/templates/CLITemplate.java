90c88580e416c14c6cabeb4b8c42e811f00e0641
andreasantarsiero <andrea@aka-Slayer>
2025-05-26 21:10:56 +0200

    working on cli

--- src/main/java/it/polimi/ingsw/gc11/view/cli/templates/CLITemplate.java
+++ src/main/java/it/polimi/ingsw/gc11/view/cli/templates/CLITemplate.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.gc11.view.cli.templates;
 import it.polimi.ingsw.gc11.view.*;
 import it.polimi.ingsw.gc11.view.cli.InputHandler;
 import it.polimi.ingsw.gc11.view.cli.MainCLI;
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+import java.util.List;
 
 
 
@@ -10,6 +13,7 @@ public abstract class CLITemplate extends Template {
 
     protected final MainCLI mainCLI;
     protected InputHandler inputHandler;
+    protected String serverMessage;
 
 
 
@@ -49,8 +53,35 @@ public abstract class CLITemplate extends Template {
 
 
 
-    public void clearView() {
+    protected void clearView() {
         System.out.print("\u001b[H\u001b[2J");
         System.out.flush();
+    }
+
+
+
+    protected void renderMenu(String title, List<String> options, int selected) {
+        if (title != null && !title.isEmpty()) {
+            System.out.println(title);
+        }
+        for (int i = 0; i < options.size(); i++) {
+            if (i == selected) {
+                AttributedString highlighted = new AttributedString(
+                        "  > " + options.get(i),
+                        AttributedStyle.DEFAULT.background(235)
+                );
+                System.out.println(highlighted.toAnsi());
+            } else {
+                System.out.println("    " + options.get(i));
+            }
+        }
+    }
+
+
+
+    protected void renderIntegerChoice(String label, int value) {
+        String line = label + ": " + value;
+        AttributedString highlighted = new AttributedString(line, AttributedStyle.DEFAULT.background(235));
+        System.out.print(highlighted.toAnsi());
     }
 }
