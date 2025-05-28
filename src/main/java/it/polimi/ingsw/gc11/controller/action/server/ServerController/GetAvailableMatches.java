444e579eaf7e2e24a3b6f66c19fdd7ea76d728fd
andreasantarsiero <andrea@aka-Slayer>
2025-05-28 17:25:40 +0200

    working on cli

--- src/main/java/it/polimi/ingsw/gc11/controller/action/server/ServerController/GetAvailableMatches.java
+++ src/main/java/it/polimi/ingsw/gc11/controller/action/server/ServerController/GetAvailableMatches.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.gc11.controller.action.server.ServerController;
 
 import it.polimi.ingsw.gc11.controller.ServerController;
 import it.polimi.ingsw.gc11.controller.action.client.NotifyExceptionAction;
-import it.polimi.ingsw.gc11.controller.action.client.NotifySuccessAction;
 import it.polimi.ingsw.gc11.controller.action.client.SendAvailableMatchesAction;
 import it.polimi.ingsw.gc11.exceptions.NetworkException;
 import java.util.List;
@@ -23,9 +22,6 @@ public class GetAvailableMatches extends ClientControllerAction {
             Map<String, List<String>> availableMatches = serverController.getAvailableMatches(username, token);
             SendAvailableMatchesAction response = new SendAvailableMatchesAction(availableMatches);
             serverController.sendAction(username, response);
-
-            NotifySuccessAction success = new NotifySuccessAction();
-            serverController.sendAction(username, success);
         }
         catch (Exception e) {
             NotifyExceptionAction exception = new NotifyExceptionAction(e.getMessage());
