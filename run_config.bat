61166b5018e00a000578d9821a0dbbad2912ab5a
andreasantarsiero <andrea@aka-Slayer>
2025-06-02 16:31:47 +0200

    run config modified

--- run_config.bat
+++ run_config.bat
@@ -4,10 +4,16 @@
 chcp 65001
 
 :: Start server
-start cmd /k "java -jar target\GC11-1.0-SNAPSHOT-jar-with-dependencies.jar -server"
+start cmd /k "java -jar target\GC11-1.0-SNAPSHOT-jar-with-dependencies.jar -s"
 
 :: Start first CLI client
 start cmd /k "java -jar target\GC11-1.0-SNAPSHOT-jar-with-dependencies.jar -cli"
 
 :: Start second CLI client
+start cmd /k "java -jar target\GC11-1.0-SNAPSHOT-jar-with-dependencies.jar -cli"
+
+:: Start third CLI client
+start cmd /k "java -jar target\GC11-1.0-SNAPSHOT-jar-with-dependencies.jar -cli"
+
+:: Start fourth CLI client
 start cmd /k "java -jar target\GC11-1.0-SNAPSHOT-jar-with-dependencies.jar -cli"
