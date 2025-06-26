e9161933b75c92c8c2ba76d8b82ad865b054c8f6
Luca Sartori <luca.sartori1@outlook.com>
2025-06-27 01:30:50 +0200

    javadoc

--- src/main/java/it/polimi/ingsw/gc11/action/client/UpdatePlayersColorAction.java
+++ src/main/java/it/polimi/ingsw/gc11/action/client/UpdatePlayersColorAction.java
@@ -4,31 +4,62 @@ import it.polimi.ingsw.gc11.view.*;
 import java.util.Map;
 
 
-
+/**
+ * Action that updates the mapping of player identifiers
+ * to their assigned display colors during the Joining phase.
+ */
 public class UpdatePlayersColorAction extends ServerAction {
 
     private final Map<String, String> playersColor;
 
-
+    /**
+     * Constructs a new UpdatePlayersColorAction.
+     *
+     * @param playersColor a map of player identifiers to color strings
+     */
     public   UpdatePlayersColorAction(Map<String, String> playersColor) {
         this.playersColor = playersColor;
     }
 
-
+    /**
+     * Loads the players’ color mapping into the Joining phase data.
+     *
+     * @param joiningPhaseData the data model for the Joining phase
+     */
     @Override
     public void loadData(JoiningPhaseData joiningPhaseData) {
         joiningPhaseData.setPlayersColor(playersColor);
     }
 
+    /**
+     * No-op for the Building phase.
+     *
+     * @param buildingPhaseData the data model for the Building phase
+     */
     @Override
     public void loadData(BuildingPhaseData buildingPhaseData) {}
 
+    /**
+     * No-op for the Check phase.
+     *
+     * @param checkPhaseData the data model for the Check phase
+     */
     @Override
     public void loadData(CheckPhaseData checkPhaseData) {}
 
+    /**
+     * No-op for the Adventure phase.
+     *
+     * @param adventurePhaseData the data model for the Adventure phase
+     */
     @Override
     public void loadData(AdventurePhaseData adventurePhaseData) {}
 
+    /**
+     * No-op for the End phase.
+     *
+     * @param endPhaseData the data model for the End phase
+     */
     @Override
     public void loadData(EndPhaseData endPhaseData) {}
 }
--- src/main/java/it/polimi/ingsw/gc11/action/client/UpdateShipBoardAction.java
+++ src/main/java/it/polimi/ingsw/gc11/action/client/UpdateShipBoardAction.java
@@ -3,35 +3,72 @@ package it.polimi.ingsw.gc11.action.client;
 import it.polimi.ingsw.gc11.model.shipboard.ShipBoard;
 import it.polimi.ingsw.gc11.view.*;
 
-
+/**
+ * Action that updates the ShipBoard view in both the
+ * Building and Check phases.
+ */
 public class UpdateShipBoardAction extends ServerAction{
     private final ShipBoard shipBoard;
 
+    /**
+     * Constructs a new UpdateShipBoardAction.
+     *
+     * @param shipBoard the ShipBoard to set in the view data
+     */
     public UpdateShipBoardAction(ShipBoard shipBoard){
         this.shipBoard = shipBoard;
     }
 
+    /**
+     * Returns the ShipBoard carried by this action.
+     *
+     * @return the ShipBoard instance
+     */
     public ShipBoard getShipBoard() {
         return shipBoard;
     }
 
-
+    /**
+     * No-op for the Joining phase.
+     *
+     * @param joiningPhaseData the data for the Joining phase
+     */
     @Override
     public void loadData(JoiningPhaseData joiningPhaseData) {}
 
+    /**
+     * Sets the ShipBoard in the Building phase data.
+     *
+     * @param buildingPhaseData the data for the Building phase
+     */
     @Override
     public void loadData(BuildingPhaseData buildingPhaseData) {
         buildingPhaseData.setShipBoard(shipBoard);
     }
 
+    /**
+     * Sets the ShipBoard in the Check phase data.
+     *
+     * @param checkPhaseData the data for the Check phase
+     */
     @Override
     public void loadData(CheckPhaseData checkPhaseData) {
         checkPhaseData.setShipBoard(shipBoard);
     }
 
+    /**
+     * No-op for the Adventure phase.
+     *
+     * @param adventurePhaseData the data for the Adventure phase
+     */
     @Override
     public void loadData(AdventurePhaseData adventurePhaseData) {}
 
+    /**
+     * No-op for the End phase.
+     *
+     * @param endPhaseData the data for the End phase
+     */
     @Override
     public void loadData(EndPhaseData endPhaseData) {}
 }
