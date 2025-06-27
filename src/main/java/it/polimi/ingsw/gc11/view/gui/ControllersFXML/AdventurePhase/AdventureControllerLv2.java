3a319b99fa1e6b828e0aba659b44e236cbcdddfd
lorenzostani <lorenzo.stani.03@gmail.com>
2025-06-27 11:23:34 +0200

    working on GUI

--- src/main/java/it/polimi/ingsw/gc11/view/gui/ControllersFXML/AdventurePhase/AdventureControllerLv2.java
+++ src/main/java/it/polimi/ingsw/gc11/view/gui/ControllersFXML/AdventurePhase/AdventureControllerLv2.java
@@ -6,6 +6,8 @@ import it.polimi.ingsw.gc11.model.Player;
 import it.polimi.ingsw.gc11.model.adventurecard.*;
 import it.polimi.ingsw.gc11.view.AdventurePhaseData;
 import it.polimi.ingsw.gc11.view.Controller;
+import it.polimi.ingsw.gc11.view.EndPhaseData;
+import it.polimi.ingsw.gc11.view.gui.ControllersFXML.EndGamePhase.EndGameController;
 import it.polimi.ingsw.gc11.view.gui.MainGUI;
 import it.polimi.ingsw.gc11.view.gui.ViewModel;
 import javafx.application.Platform;
@@ -725,5 +727,26 @@ public class AdventureControllerLv2 extends Controller {
         });
     } //ok
 
+    @Override
+    public void change() {
+        Platform.runLater(() -> {
 
+            ViewModel viewModel = (ViewModel) stage.getUserData();
+            EndPhaseData adventurePhaseData = (EndPhaseData) viewModel.getPlayerContext().getCurrentPhase();
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(MainGUI.class
+                        .getResource("/it/polimi/ingsw/gc11/gui/AdventurePhase/Endgame.fxml"));
+                Scene newScene = new Scene(fxmlLoader.load(), 1280, 720);
+                EndGameController controller = fxmlLoader.getController();
+                adventurePhaseData.setListener(controller);
+                controller.init(stage);
+                stage.setScene(newScene);
+                stage.show();
+            }
+            catch (Exception e) {
+                System.out.println("FXML Error: " + e.getMessage());
+            }
+
+        });
+    }
 }
