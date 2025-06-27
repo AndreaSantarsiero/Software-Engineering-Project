781e6a2c996a07fa23926fc5a9b2b1ae1d50c23c
lorenzostani <lorenzo.stani.03@gmail.com>
2025-06-27 10:38:56 +0200

    working on GUI

--- src/main/java/it/polimi/ingsw/gc11/view/gui/ControllersFXML/EndGamePhase/EndGameController.java
+++ src/main/java/it/polimi/ingsw/gc11/view/gui/ControllersFXML/EndGamePhase/EndGameController.java
@@ -1,7 +1,35 @@
 package it.polimi.ingsw.gc11.view.gui.ControllersFXML.EndGamePhase;
 
+import it.polimi.ingsw.gc11.controller.State.EndGamePhase;
+import it.polimi.ingsw.gc11.network.client.VirtualServer;
 import it.polimi.ingsw.gc11.view.Controller;
+import it.polimi.ingsw.gc11.view.EndPhaseData;
+import it.polimi.ingsw.gc11.view.JoiningPhaseData;
+import it.polimi.ingsw.gc11.view.gui.ViewModel;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+
 
 public class EndGameController extends Controller {
 
+    private Stage stage;
+    private EndPhaseData data;
+
+
+    @FXML HBox pointText;
+
+    public void init(Stage stage) {
+        this.stage = stage;
+        ViewModel viewModel = (ViewModel) this.stage.getUserData();
+        VirtualServer virtualServer = viewModel.getVirtualServer();
+        this.data = (EndPhaseData) viewModel.getPlayerContext().getCurrentPhase();
+
+       pointText.getChildren().add(new Label(
+               "You have " + data.getPlayer().getCoins() + " points!"
+       ));
+    }
 }
